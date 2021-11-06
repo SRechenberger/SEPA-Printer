872a79e4aa105ace52aea58d4d229437a618d82d
Sascha Rechenberger <Sascha Rechenberger@SRECHENBERGER-P>
2021-11-06 13:12:23 +0100

    Projektdateien hinzufügen.

---
+++ SEPA.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace SEPA_Printer
+{
+    partial class SEPA
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(314, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Location = new System.Drawing.Point(332, 11);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(75, 23);
+            this.btnPrint.TabIndex = 1;
+            this.btnPrint.Text = "Drucken";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.BtnPrint_Click);
+            // 
+            // SEPA
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(417, 47);
+            this.Controls.Add(this.btnPrint);
+            this.Controls.Add(this.textBox1);
+            this.Name = "SEPA";
+            this.Text = "SEPA Drucker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnPrint;
+    }
+}
+
---
+++ SEPA.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SEPA_Printer
+{
+    public partial class SEPA : Form
+    {
+        private const int Y_RES = 100;
+
+        private const int READ_HEIGHT = 273;
+        private const int BLOCK_HEIGHT = READ_HEIGHT / 8;
+        private const int LINE_HEIGHT = BLOCK_HEIGHT / 2;
+        private const int FONT_SIZE = LINE_HEIGHT * Y_RES / 100;
+        private const int BLOCK_WIDTH = 20;
+        private const int BLOCK_WIDTH_IBAN = 16;
+        private const int BLOCKS = 27;
+        private const int BLOCKS_IBAN = 34;
+
+        public SEPA()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnPrint_Click(object sender, EventArgs e)
+        {
+
+            PrintDialog pDialog = new PrintDialog();
+            DialogResult printing = pDialog.ShowDialog();
+
+            PaperSize sepaSize = new PaperSize("SEPA Überweisungsträger", 416, 590);
+
+            if (printing == DialogResult.OK) 
+            {
+                Font printFont = new Font("Courier New", FONT_SIZE);
+                PrintDocument pd = new PrintDocument();
+
+                pd.DefaultPageSettings.Margins.Top = 69;
+                pd.DefaultPageSettings.Margins.Bottom = 77;
+                pd.DefaultPageSettings.Margins.Left = 30;
+                pd.DefaultPageSettings.Margins.Right = 30;
+                pd.DefaultPageSettings.PaperSize = sepaSize;
+                pd.DefaultPageSettings.PrinterResolution.X = 100;
+                pd.DefaultPageSettings.PrinterResolution.Y = 100;
+
+                pd.PrinterSettings.PrinterName = pDialog.PrinterSettings.PrinterName;
+                pd.PrintPage += new PrintPageEventHandler((object lsender, PrintPageEventArgs ev) =>
+                {
+                    String text = this.textBox1.Text;
+                    float xPos = ev.MarginBounds.Left;
+                    float yPos = (float)(ev.MarginBounds.Top);
+                    ev.Graphics.DrawRectangle(new Pen(Brushes.Black), 0, 0, 10, 10);
+                    for (int line = 0; line < 8; line++)
+                    {
+                        for (int block = 0; block < BLOCKS; block++)
+                        {
+                            try
+                            {
+                                ev.Graphics.DrawString(text.Substring(block, 1), printFont, Brushes.Black, xPos + block * BLOCK_WIDTH, yPos + line * LINE_HEIGHT * 2, new StringFormat());
+                            } catch(ArgumentOutOfRangeException ex)
+                            {
+                                // Do Nothing
+                            }
+                        }
+                    }
+                    ev.HasMorePages = false;
+                });
+
+                pd.Print();
+            }
+        }
+    }
+}
